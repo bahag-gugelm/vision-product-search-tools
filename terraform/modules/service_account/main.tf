c2e26056adb8b940caa3da76dc8ed50c744b4bc3
Mike Gugel <mikhail.gugel@bahag.com>
2024-06-10 13:54:05 +0200

    ci: permissions to the sa

--- terraform/modules/service_account/main.tf
+++ terraform/modules/service_account/main.tf
@@ -2,3 +2,16 @@ resource "google_service_account" "pc_vision_product_search_tools_sa" {
   account_id   = "pc-vision-product-search-tools"
   display_name = "pc-vision-product-search-tools"
 }
+
+resource "google_project_iam_binding" "pc_vision_product_search_tools_sa" {
+  project = "${var.project_id}"
+  for_each = toset([
+    "roles/storage.objectCreator",
+    "roles/storage.objectViewer",
+    "roles/storage.admin",
+  ])
+  role = each.key
+  members = [
+    "serviceAccount:${google_service_account.pc_vision_product_search_tools_sa.email}"
+  ]
+}
